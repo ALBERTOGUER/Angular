84580d10aa5aa0633297be36479c54f0964adfdd
unknown <alberto.perez.1509@outlook.com>
2019-10-22 09:35:45 +0200

    ejemplo2 a la mitad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent} from './components/navbar/navbar.component';
+import { PresentacionComponent} from './components/presentacion/presentacion.component'
+import { ServiciosComponent} from './components/servicios/servicios.component'
+  
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PresentacionComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule
